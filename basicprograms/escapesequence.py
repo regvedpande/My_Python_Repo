cacc76a14c80e6c42421999c1ef6cb0e9e540cbc
regvedpande <149050058+regvedpande@users.noreply.github.com>
2025-01-03 10:55:01 +0530

    Some Work

--- basicprograms/escapesequence.py
+++ basicprograms/escapesequence.py
@@ -1,1 +1,2 @@
 print("This is Google\nThis is the richest company in the World")
+print("\"This is Google\" and \"This is Apple\" and \'This is Microsoft\'")
---
+++ basicprograms/separator.py
@@ -0,0 +1,2 @@
+print("\"Python\" is used for Machine Learning", sep='~', end='\nThis is \t')
+print("Google")
