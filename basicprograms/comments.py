20e44d8605591c454a452afbe2b13f1e175b582e
regvedpande <149050058+regvedpande@users.noreply.github.com>
2025-01-03 10:43:25 +0530

    Some Practice

---
+++ basicprograms/comments.py
@@ -0,0 +1,9 @@
+#This is a single line comment
+'''
+This is Multi line comment
+'''
+
+"""
+This is also a multi line comment
+"""
+print("Apple is the richest company in the World")
---
+++ basicprograms/escapesequence.py
@@ -0,0 +1,1 @@
+print("This is Google\nThis is the richest company in the World")
