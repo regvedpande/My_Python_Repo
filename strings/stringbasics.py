28cd3b2eda9b2306ec8f44f398f98a44152c5e13
regvedpande <149050058+regvedpande@users.noreply.github.com>
2025-01-06 16:50:26 +0530

    some string practice

---
+++ strings/stringbasics.py
@@ -0,0 +1,14 @@
+name = "Regved"
+name2 = '''"Regved" is my Name'''
+print(name)
+print(name2)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+# Using for loop to print each character of the string
+for i in name:
+    print(i)
