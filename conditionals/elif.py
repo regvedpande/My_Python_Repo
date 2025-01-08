8a58f3dbf62b6cf5bcba3983b818a8f2daa888c8
regvedpande <149050058+regvedpande@users.noreply.github.com>
2025-01-08 16:01:45 +0530

    conditionals practice

---
+++ conditionals/elif.py
@@ -0,0 +1,7 @@
+enter = int(input("Enter your Age : "))
+if(enter < 18):
+    print("You Cannot Drive")
+elif(enter==18):
+    print("You Can Maybe Drive")  
+else:
+    print("You Can Drive")      
---
+++ conditionals/ifelse.py
@@ -0,0 +1,5 @@
+enter = int(input("Enter your Age : "))
+if(enter >= 18):
+    print("You Can Vote")
+else:
+    print("You Cannot Vote")    
