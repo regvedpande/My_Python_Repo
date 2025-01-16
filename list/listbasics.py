dfbe07353d0180430e6f593bca2d72cf017a431e
regvedpande <regregd@outlook.com>
2025-01-16 15:31:09 +0530

    List Practice

---
+++ list/listbasics.py
@@ -0,0 +1,14 @@
+fav = ["regved", 25, "delia", 28, "ayush", 20]
+print(fav)
+print(fav[0])
+print(fav[1])   
+print(fav[2])
+print(fav[3])
+
+#Negative to positive index
+print(fav[-3])
+print(fav[len(fav)-3])
+if "delia" in fav:
+    print("Delia is in the list")
+else:
+    print("Delia is not in the list")
