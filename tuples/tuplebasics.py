d5b78200ef9c95de4f295bcea959108a1f44cdd0
regvedpande <regregd@outlook.com>
2025-01-17 10:58:56 +0530

    Some Practice

---
+++ tuples/tuplebasics.py
@@ -0,0 +1,24 @@
+tup = (12, 34, 56, 78, 90)
+# Count how many times a value appears in the tuple
+print("Count of 34:", tup.count(34))
+
+# Find the index/position of a value in the tuple
+print("Index of 56:", tup.index(56))
+
+# Get length of the tuple
+print("Length:", len(tup))
+
+# Convert tuple to a list
+print("As list:", list(tup))
+
+# Get minimum value in tuple 
+print("Min value:", min(tup))
+
+# Get maximum value in tuple
+print("Max value:", max(tup))
+
+# Get sum of all values in tuple
+print("Sum:", sum(tup))
+
+# Check if value exists in tuple
+print("Is 90 present?", 90 in tup)
