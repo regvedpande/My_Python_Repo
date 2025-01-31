f6d0f1ad0cc8f55b1e7d54698bb6b78596f2a8cf
regvedpande <regregd@outlook.com>
2025-01-31 15:12:06 +0530

    Some Changes

--- exceptionhandling/exceptionbasics.py
+++ exceptionhandling/exceptionbasics.py
@@ -2,7 +2,7 @@ a = input("Enter a number: ")
 print(f"Multiplication table of {a} is:")
 
 try:
-   for i in range(1, 11):
-    print(f"{int(a)} x {i} = {int(a)*i}") 
-except:
+    for i in range(1, 11):
+        print(f"{int(a)} x {i} = {int(a)*i}")
+except ValueError as e:
     print(f"Error: {e}")
