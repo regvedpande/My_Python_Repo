83fb3f3e4ab7217e806f22d15faa6c974717cbab
regvedpande <regregd@outlook.com>
2025-02-07 14:22:40 +0530

    Some Changes

--- basicprograms/escapesequence.py
+++ basicprograms/escapesequence.py
@@ -1,2 +1,2 @@
 print("This is Google\nThis is the richest company in the World")
-print("\"This is Google\" and \"This is Apple\" and \'This is Microsoft\'")
+print("\"This is Google\" and \"This is Apple\" and \'This is Microsoft\' and \'This is Amazon\'")
