30c420716d95438a29251390c94f15dca3b734e8
regvedpande <regregd@outlook.com>
2025-04-25 18:23:54 +0530

    work

---
+++ strings/doctwo.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    """
+    Add two numbers and return the result.
+
+    Parameters:
+    a (int): First number
+    b (int): Second number
+
+    Returns:
+    int: The sum of a and b
+    """
+    return a + b
+
+print(help(add))
