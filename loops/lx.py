5d05dbbfde2526fcca21aa00ad8f7482db767d1b
Regved Pande <149050058+regvedpande@users.noreply.github.com>
2025-05-27 18:26:34 +0530

    Create lx.py

---
+++ loops/lx.py
@@ -0,0 +1,11 @@
+# Create a list of fruits
+fruits = ["apple", "banana", "cherry", "orange", "kiwi"]
+
+print("Here are my favorite fruits:")
+
+# Loop through each fruit in the 'fruits' list
+for fruit in fruits:
+  # Capitalize the first letter of each fruit and print a message
+  print(f"I love {fruit.capitalize()}!")
+
+print("\nThat's all my favorite fruits!")
