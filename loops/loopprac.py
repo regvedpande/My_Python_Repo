f3ccc54593ada507c68cf65edd64c89d966d9639
Regved Pande <149050058+regvedpande@users.noreply.github.com>
2025-05-30 22:16:51 +0530

    Some contributions

---
+++ loops/loopprac.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total_sum = 0
+
+# Loop through numbers 1 to 10 and add to sum
+for i in range(1, 11):
+    total_sum += i
+
+# Print the sum
+print("Sum of numbers from 1 to 10:", total_sum)
