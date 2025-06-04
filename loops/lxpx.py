4156cbe2639c984ca1901f88b4da15547ae58216
Regved Pande <149050058+regvedpande@users.noreply.github.com>
2025-06-04 16:18:25 +0530

    Create lxpx.py

---
+++ loops/lxpx.py
@@ -0,0 +1,12 @@
+# Loop through a list
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
+
+# Loop through a string
+for char in 'hello':
+    print(char)
+
+# Loop through a range of numbers
+for i in range(5):
+    print(i)
