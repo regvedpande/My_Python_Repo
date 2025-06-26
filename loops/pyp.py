e30e8e82a4c8a139cfb8fbf43fcc3c372c73aa94
Regved Pande <149050058+regvedpande@users.noreply.github.com>
2025-06-26 23:23:03 +0530

    Some Work

---
+++ loops/pyp.py
@@ -0,0 +1,9 @@
+# This code iterates through a list of fruits and prints each one.
+
+fruits = ["apple", "banana", "cherry", "date"]
+
+print("Here are your favorite fruits:")
+for fruit in fruits:
+    print(fruit)
+
+print("\nLoop finished!")
