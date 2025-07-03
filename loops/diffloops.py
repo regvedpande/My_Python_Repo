a09699244f6b150f96c1a9751534a66c7580fb80
Regved Pande <149050058+regvedpande@users.noreply.github.com>
2025-07-03 18:42:11 +0530

    Work

---
+++ loops/diffloops.py
@@ -0,0 +1,17 @@
+# Using a for loop to print numbers from 1 to 5
+print("Using for loop:")
+for i in range(1, 6):
+    print(i)
+
+# Using a while loop to do the same
+print("\nUsing while loop:")
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+# Looping through a list
+print("\nLooping through a list:")
+fruits = ["apple", "banana", "cherry", "date"]
+for fruit in fruits:
+    print(fruit)
